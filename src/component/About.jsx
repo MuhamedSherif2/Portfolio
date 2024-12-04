a953debc2e1116b3deb766ccdf75ef0987d8da53
mohammed <mohammedsherif675@gamil.com>
2024-12-04 11:36:36 +0200

    skills

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -4,15 +4,15 @@ import img from '../assets/IMG-20240908-WA0092.jpg'
 function About() {
   return (
     <section className='about d-flex'>
-      <div className='container d-flex align-items-center'>
-        <div className='w-50'>
+      <div className='container d-flex flex-column flex-lg-row flex-md-row align-items-center pt-5 '>
+        <div className=''>
           <h3 className='fs-2 fw-bolder'>Hello, It's Me <br /> <span>Mohammed Sherif</span> <br /> And i'm </h3>
           <p className='fs-4 '>I'm a web Designer with experience for over 1 year.
             <br />expertise is to creat and website desing, front end design.
           </p>
         </div>
 
-        <div className='w-50 d-flex justify-content-center '>
+        <div className='d-flex justify-content-center'>
           <img src={img} className='about-img' />
         </div>
       </div>
--- src/component/Skills.jsx
+++ src/component/Skills.jsx
@@ -5,10 +5,34 @@ function Skills() {
         <section className="skills">
             <div className="container pt-5">
                 <h2 className='fs-2 d-flex justify-content-center fw-bolder'>Skills</h2>
-                <div className='d-flex'>
-                    <h2>HTML :</h2>
-                    <div class="progress bg-info" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
-                        <div class="progress-bar" style={{ width: "75%" }}></div>
+                <div className='mt-5'>
+                    <h2>HTML: 90%</h2>
+                    <div className="progress" role="progressbar" aria-label="Basic example" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style={{backgroundColor: '#493628'}}>
+                        <div className="progress-bar" style={{ width: "90%", backgroundColor:'#E4E0E1' }}></div>
+                    </div>
+                </div>
+                <div className='mt-5'>
+                    <h2>CSS: 90%</h2>
+                    <div className="progress" role="progressbar" aria-label="Basic example" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style={{backgroundColor: '#493628'}}>
+                        <div className="progress-bar" style={{ width: "90%", backgroundColor:'#E4E0E1' }}></div>
+                    </div>
+                </div>
+                <div className='mt-5'>
+                    <h2>Bootstrap: 90%</h2>
+                    <div className="progress" role="progressbar" aria-label="Basic example" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style={{backgroundColor: '#493628'}}>
+                        <div className="progress-bar" style={{ width: "90%", backgroundColor:'#E4E0E1' }}></div>
+                    </div>
+                </div>
+                <div className='mt-5'>
+                    <h2>Java-Script: 90%</h2>
+                    <div className="progress" role="progressbar" aria-label="Basic example" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style={{backgroundColor: '#493628'}}>
+                        <div className="progress-bar" style={{ width: "90%", backgroundColor:'#E4E0E1' }}></div>
+                    </div>
+                </div>
+                <div className='mt-5'>
+                    <h2>React JS: 90%</h2>
+                    <div className="progress" role="progressbar" aria-label="Basic example" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style={{backgroundColor: '#493628'}}>
+                        <div className="progress-bar" style={{ width: "90%", backgroundColor:'#E4E0E1' }}></div>
                     </div>
                 </div>
 
