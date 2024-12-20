1eff649f34b6dc52aa418e660fb32e614f83444a
mohammed <mohammedsherif675@gamil.com>
2024-12-20 22:04:10 +0200

    about

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -6,7 +6,7 @@ function About() {
     <section className='about d-flex pt-4'>
       <div className='container d-flex flex-column flex-lg-row flex-md-row align-items-center pt-5 '>
         <div className=''>
-          <h3 className='fs-2 fw-bolder'>Hello, It's Me <br /> <span>Mohammed Sherif</span> <br /> And i'm </h3>
+          <h3 className='fs-2 fw-bolder'>Hello, It's Me <br /> <span>Mohammed Sherif</span> <br /> And i'm React JS Developer </h3>
           <p className='fs-4 '>I'm a web Designer with experience for over 1 year.
             <br />expertise is to creat and website desing, front end design.
           </p>
